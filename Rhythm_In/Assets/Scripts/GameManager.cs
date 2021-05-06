6a27cac1707904818a9039390c1bfb8378fba6d2
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-05-07 06:04:35 +0900

    기본기능 구현
    1. 맵 자동생성
    2. 캐릭터 이동로직, 공격로직, 데미지 입었을 때 로직, 애니메이션
    3. 적 죽을 때 로직, 애니메이션

--- Rhythm_In/Assets/Scripts/GameManager.cs
+++ Rhythm_In/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +8,34 @@ public class GameManager : MonoBehaviour
     public PlayerMove player;
     public GameObject playerObj;
     public GameObject Maps;
-    
+    public GameObject grid;
+    private void Update()
+    {
+        CreateMap();
+        
+    }
 
+    //생성된 맵 카운터
+    private int createdMapCnt = 1;
+
+    //맵 자동생성 메소드
     public void CreateMap()
     {
-        if (playerObj.transform.position.x > 10)
+        
+        if (playerObj.transform.position.x / 10f >= createdMapCnt)
         {
-            Vector3 point = new Vector3(playerObj.transform.position.x, 0f, 0f);
-            Instantiate(Maps,point,Quaternion.identity);
+            createdMapCnt++;
+            
+            //맵 생성 포인트를 현재 플레이어의 포지션.X +5 로 함 
+            Vector3 point = new Vector3(playerObj.transform.position.x + 5f, 0f, 0f);
+            //타일맵을 Grid 오브젝트의 자식으로 생성
+            Instantiate(Maps, point, Quaternion.identity).transform.parent = grid.transform;
+
         }
+        
+        
+        
     }
+
+
 }
--- Rhythm_In/Assets/Scripts/PlayerMove.cs
+++ Rhythm_In/Assets/Scripts/PlayerMove.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -9,94 +10,128 @@ public class PlayerMove : MonoBehaviour
     private float moveSpeed=10f;
     [SerializeField]
     private float jumpPower;
-
     public GameManager GM;
+    public float hitbox;
     
     Rigidbody2D rigid;
     SpriteRenderer spRenderer;
     Animator anim;
     
-    
+    //애니메이션 변수 id
     private static readonly int IsJumping = Animator.StringToHash("isJumping");
+    private static readonly int IsRun = Animator.StringToHash("isRun");
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         spRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
     }
 
+    //더블 점프 카운터
+    private int isDoubleJump = 0;
     void Update()
     {
-        Vector2 direction = new Vector2(1f, 0f);
-        rigid.position += direction * (moveSpeed * Time.deltaTime);
-        GM.CreateMap();
+        //씬 리셋
+        if (Input.GetKey(KeyCode.R))
+            SceneManager.LoadScene(0);
+        
+        //이동 애니메이션
+        if (rigid.velocity.x < 1f)
+            anim.SetBool(IsRun, false);
+        else
+            anim.SetBool(IsRun, true);
+
+        
+        //공격 로직
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            anim.SetTrigger("doAttack1");
+            
+            //플레이어 포지션 기준으로 X축 방향으로 RAY를 쏴줌, Enemy에게만 맞도록 layer 설정
+            RaycastHit2D raycastHit2D = 
+                Physics2D.Raycast(rigid.position, Vector2.right, hitbox, LayerMask.GetMask("Enemy"));
+            Debug.DrawRay(rigid.position, Vector2.right * hitbox, new Color(1, 1, 1));
+            
+            //레이가 감지되면 맞은 오브젝트의 die 애니메이션 재생
+            //콜라이더를 비활성화 시켜줌으로써 플레이어에게 피해 없음
+            //애니메이션이 끝난 뒤(1초 뒤)에 오브젝트 삭제
+            if (raycastHit2D)
+            {
+                //Debug.Log(raycastHit2D.transform.name);
+                raycastHit2D.transform.GetComponent<Animator>().SetTrigger("doHitDie");
+                raycastHit2D.collider.enabled = false;
+                StartCoroutine(OnDestroyEnemy(raycastHit2D));
+            }
+        }
     }
 
-    
-    
-    /*
-    private void Update()
+    IEnumerator OnDestroyEnemy(RaycastHit2D raycastHit2D)
     {
-        //Jump
-        if (Input.GetButton("Jump"))
+        yield return new WaitForSeconds(1);
+        Destroy(raycastHit2D.transform.gameObject);
+    }
+
+    private void FixedUpdate()
+    {
+        //이동
+        rigid.AddForce(Vector2.right * moveSpeed);
+        
+        //점프 & 더블 점프
+        if (Input.GetButtonDown("Jump") && isDoubleJump < 2)
         {
+            isDoubleJump++;
+            
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             anim.SetBool(IsJumping, true);
         }
-        
-        //방향키에서 손을 떼면 극적으로 스탑
-        if (Input.GetButtonUp("Horizontal"))
-        {
-            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
-        }
-
-        if (Input.GetButton("Horizontal"))
-        {
-            //flipX는 bool 변수, 왼쪽 방향키를 눌렀다면 flipX=true
-            spRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
-        }
-
-        //달리기 애니메이션
-        if(Mathf.Abs(rigid.velocity.x) < 0.6f)
-        {
-            anim.SetBool("isRun", false);
-        }
-        else
-        {
-            anim.SetBool("isRun", true);
-        }
     }
-    */
-    
-    /*
-    // Update is called once per frame
-    void FixedUpdate()
+
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        //Move Speed
-        float h = Input.GetAxisRaw("Horizontal");
+        //바닥 착지시 점프 애니메이션 중지
+        if(other.transform.CompareTag("Ground"))
+            anim.SetBool(IsJumping, false);
+        //더블 점프 카운트 초기화
+        isDoubleJump = 0;
 
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-        
-        //Max Speed
-        if(rigid.velocity.x > maxSpeed)
+        if (other.transform.CompareTag("Enemy"))
         {
-            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-        }
-        else if (rigid.velocity.x < (maxSpeed * -1f) )
-        {
-            rigid.velocity = new Vector2((maxSpeed * -1f), rigid.velocity.y);
-        }
-        
-        //Landing Platform
-        Debug.DrawRay(rigid.position, Vector3.down, Color.green);
-
-        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1);
-
-        if (rayHit.collider != null)
-        {
-            Debug.Log(rayHit.collider.name);
+            OnDamaged(other.transform.position);
         }
     }
-    */
+
+    //적에게 부딛혔을 때
+    void OnDamaged(Vector2 targetPos)
+    {
+        //6=PlayerDamaged
+        gameObject.layer = 6;
+
+        //무적 상태일 때 티가 나도록 알파 값을 낮춰줌
+        spRenderer.color = new Color(1, 1, 1, 0.7f);
+
+        
+        int damDirc = 0;
+        //플레이어 포지션과 enemy 포지션의 차를 이용해서 플레이어가 튕겨져 나갈 방향 계산
+        if ((transform.position.x - targetPos.x) > 0) damDirc = 1;
+        else damDirc = -1;
+
+        //위 계산으로 나온 방향으로 플레이어에게 힘을 가함
+        rigid.AddForce(new Vector2(damDirc, 0f) * 40f, ForceMode2D.Impulse);
+        
+        anim.SetTrigger("doDamaged");
+        
+        //2초간 무적상태
+        Invoke("OffDamaged",2f);
+    }
+
+    //무적 판정
+    void OffDamaged()
+    {
+        gameObject.layer = 0;
+        spRenderer.color = new Color(1, 1, 1, 1);
+    }
 }
