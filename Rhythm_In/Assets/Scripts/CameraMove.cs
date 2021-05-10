9dc52c3426fbfb8f81c1ef7c4b9238de6bb3584d
Mellow1213 <dpcks7282@gmail.com>
2021-05-10 16:39:09 +0900

    카메라 Y축 약간 이동 변경

--- Rhythm_In/Assets/Scripts/CameraMove.cs
+++ Rhythm_In/Assets/Scripts/CameraMove.cs
@@ -16,6 +16,6 @@ public class CameraMove : MonoBehaviour
     {
         // 캐릭터의 X좌표 값 따라 카메라 이동
         //카메라의 Y 값은 2로, Z값은 -0.5로 고정
-        transform.position = new Vector3(playerTransition.position.x, 2, -0.5f);
+        transform.position = new Vector3(playerTransition.position.x, playerTransition.position.y * 0.2f, -0.5f);
     }
 }
