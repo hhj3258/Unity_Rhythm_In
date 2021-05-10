e6b770dedd6d79f5796f5c564367a305be8242ca
Mellow1213 <dpcks7282@gmail.com>
2021-05-10 15:52:17 +0900

    카메라 이동 스크립트 추가
    캐릭터의 X 좌표에 따른 카메라 이동
    Y좌표는 영향을 주지 않음

---
+++ Rhythm_In/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerPosition;
+    private Transform playerTransition;
+    void Start()
+    {
+        playerTransition = playerPosition.transform;
+    }
+
+    void LateUpdate()
+    {
+        // 캐릭터의 X좌표 값 따라 카메라 이동
+        //카메라의 Y 값은 2로, Z값은 -0.5로 고정
+        transform.position = new Vector3(playerTransition.position.x, 2, -0.5f);
+    }
+}
